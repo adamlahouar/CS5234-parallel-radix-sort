b35a89bf8808e704d120b9c65c5877dfc1e5fb1d
Adam Lahouar <adamlahouar@vt.edu>
2025-05-11 00:09:12 -0400

    minor data generator modification

--- cpu/data_generator.cpp
+++ cpu/data_generator.cpp
@@ -16,7 +16,7 @@ constexpr double GAMMA_SCALE = 100'000;
 constexpr int GAMMA_MAX = 1'000'000;
 
 constexpr int BASE_SEED = 42;
-const int MAX_THREADS = std::thread::hardware_concurrency();
+const int MAX_THREADS = static_cast<int>(std::thread::hardware_concurrency());
 
 int *DataGenerator::generate(const int size, const DistributionType distType) {
     const auto data = new int[size];
@@ -32,7 +32,7 @@ int *DataGenerator::generate(const int size, const DistributionType distType) {
             case DistributionType::UNIFORM: {
                 std::uniform_int_distribution dis(UNIFORM_MIN, UNIFORM_MAX);
                 #pragma omp for
-                for (std::size_t i = 0; i < size; ++i)
+                for (int i = 0; i < size; ++i)
                     data[i] = dis(rng);
                 break;
             }
@@ -40,7 +40,7 @@ int *DataGenerator::generate(const int size, const DistributionType distType) {
             case DistributionType::NORMAL: {
                 std::normal_distribution dis(NORMAL_MEAN, NORMAL_STD);
                 #pragma omp for
-                for (std::size_t i = 0; i < size; ++i)
+                for (int i = 0; i < size; ++i)
                     data[i] = std::clamp(static_cast<int>(dis(rng)), UNIFORM_MIN, UNIFORM_MAX);
                 break;
             }
@@ -48,7 +48,7 @@ int *DataGenerator::generate(const int size, const DistributionType distType) {
             case DistributionType::SKEW_SMALL: {
                 std::gamma_distribution dis(GAMMA_SHAPE, GAMMA_SCALE);
                 #pragma omp for
-                for (std::size_t i = 0; i < size; ++i)
+                for (int i = 0; i < size; ++i)
                     data[i] = std::min(static_cast<int>(dis(rng)), GAMMA_MAX);
                 break;
             }
@@ -56,7 +56,7 @@ int *DataGenerator::generate(const int size, const DistributionType distType) {
             case DistributionType::SKEW_LARGE: {
                 std::gamma_distribution dis(GAMMA_SHAPE, GAMMA_SCALE);
                 #pragma omp for
-                for (std::size_t i = 0; i < size; ++i)
+                for (int i = 0; i < size; ++i)
                     data[i] = std::max(GAMMA_MAX - static_cast<int>(dis(rng)), UNIFORM_MIN);
                 break;
             }
